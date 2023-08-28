f56f701e743e29faa718369ff81100e381c9b1ec
BTC-LeeSoonMin <soonmin07@gmail.com>
2023-08-28 21:20:40 +0900

    feat: member_mapper.xml 추가
    user 폴더에 member_mapper.xml 추가

--- thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberController.java
+++ thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberController.java
@@ -1,12 +1,11 @@
 package com.btc.thewayhome.user.member;
 
-import lombok.extern.log4j.Log4j2;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
