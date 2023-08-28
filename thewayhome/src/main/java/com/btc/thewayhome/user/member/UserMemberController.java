1c9e869891acfef0097f9333d4d5d5cd823d61bd
BTC-LeeSoonMin <soonmin07@gmail.com>
2023-08-28 21:02:16 +0900

    pull 당기기위한 commit

--- thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberController.java
+++ thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberController.java
@@ -1,15 +1,39 @@
 package com.btc.thewayhome.user.member;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Log4j2
 @Controller
 @RequestMapping("/user/member")
 public class UserMemberController {
 
     @Autowired
     UserMemberService userMemberService;
+
+
+    @PostMapping("/user_delete_confirm")
+    public String userMemeberDeleteConfirm(HttpSession session) {
+        log.info("[UserMemberController] userMemeberDelete()");
+
+
+        String nextPage = "redirect:/member/user/member_logout_confirm";
+
+        UserMemberDto loginedUserMemberDto =
+                (UserMemberDto) session.getAttribute("loginedUserMemberDto");
+        int result = userMemberService.userMemeberDeleteConfirm(loginedUserMemberDto.getU_m_no());
+
+        if (result <= 0)
+            nextPage = "/member/user/member_modify_fail";
+
+
+        return nextPage;
+
+    }
 
 
 
--- thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberService.java
+++ thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberService.java
@@ -1,11 +1,21 @@
 package com.btc.thewayhome.user.member;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 public class UserMemberService implements IUserMemberService{
 
     @Autowired
     IUserMemberDaoMapper iUserMemberDaoMapper;
+
+    public int userMemeberDeleteConfirm(int u_m_no) {
+        log.info("[UserMemberService] userMemeberDeleteConfirm");
+
+        return iUserMemberDaoMapper.deleteUserMember(u_m_no);
+
+
+    }
 }
