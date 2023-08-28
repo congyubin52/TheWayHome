305b6223e91e270bc2854ee65da24c4a663ade27
congyubin52 <congyubin52@gamil.com>
2023-08-28 17:06:30 +0900

    feat: 폴더 구조 수정 완료
    빈 폴더 누락 해결을 위해 더미 파일 임시 생성

--- thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberController.java
+++ thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberController.java
@@ -1,4 +1,7 @@
 package com.btc.thewayhome.user.member;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserMemberController {
 }
--- thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberService.java
+++ thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberService.java
@@ -1,4 +1,11 @@
 package com.btc.thewayhome.user.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserMemberService {
+
+    @Autowired
+    IUserMemberDaoMapper iUserMemberDaoMapper;
 }
