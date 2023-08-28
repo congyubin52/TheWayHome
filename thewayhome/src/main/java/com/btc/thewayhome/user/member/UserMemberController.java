01613c0316910ae2d30a081712c62932b0ff1f20
congyubin52 <congyubin52@gamil.com>
2023-08-28 18:45:54 +0900

    feat: SpringSecurityConfig 수정
    SpringSecurityConfig 경로 오타들 수정

--- thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberController.java
+++ thewayhome/src/main/java/com/btc/thewayhome/user/member/UserMemberController.java
@@ -2,6 +2,7 @@ package com.btc.thewayhome.user.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +12,14 @@ public class UserMemberController {
     @Autowired
     UserMemberService userMemberService;
 
+    @GetMapping("/create_account_form")
+    public String createAccountForm() {
+        System.out.println("[UserMemberController] createAccountForm()");
 
+        String nextPage = "/member/user/create_account_form";
+
+        return nextPage;
+
+    }
 
 }
