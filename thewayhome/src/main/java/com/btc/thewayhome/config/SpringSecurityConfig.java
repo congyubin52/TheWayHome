c2f25e2f509b48ab795f25eb46fae63bd4be2e54
congyubin52 <congyubin52@gamil.com>
2023-08-29 17:11:16 +0900

    fix: SpringSecurityConfig, application.properties 수정
    SpringSecurityConfig user, admin 두 개의 다른 로그인 페이지에서 작동하게
    Spring Security를 구성 application.properties에서 db url, username, password 수정

--- thewayhome/src/main/java/com/btc/thewayhome/config/SpringSecurityConfig.java
+++ thewayhome/src/main/java/com/btc/thewayhome/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.DispatcherType;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,29 +16,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
-//	@Bean
-//	public PasswordEncoder getPasswordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-	
-//	@Bean
-//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//		http.cors().disable()			// CORS 방지
-//			.csrf().disable()			// CSFR 방지
-//			.formLogin().disable()		// 기본 로그인 페이지 사용 안함
-//			.headers().frameOptions().disable();
-//
-//		return http.build();
-//
-//	}
-
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
+	@Order(1)
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		log.info("filterChain");
 
@@ -58,6 +43,31 @@ public class SpringSecurityConfig {
 						.permitAll())
 				.logout()
 				.logoutUrl("/user/member/member_logout_confirm")
+				.logoutSuccessUrl("/");
+		return http.build();
+	}
+	@Bean
+	@Order(2)
+	public SecurityFilterChain filterChainAdmin(HttpSecurity http) throws Exception {
+		log.info("filterChain");
+
+		http.csrf().disable()
+				.cors().disable()
+				.authorizeHttpRequests(request -> request
+						.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+						.requestMatchers("/css/**", "/error/**", "/img/**", "/js/**", "", "/",
+								"/admin/member/create_account_form", "/admin/member/create_account_confirm").permitAll()   // Security 제외
+						.anyRequest().authenticated()
+				)
+				.formLogin(login -> login                           // 로그인 시 폼(form)을 이용
+						.loginPage("/admin/member/member_login_form")            // 로그인시 폼 주소 설정
+						.loginProcessingUrl("/member_login_confirm")
+						.usernameParameter("a_m_id")
+						.passwordParameter("a_m_pw")
+						.defaultSuccessUrl("/", true)
+						.permitAll())
+				.logout()
+				.logoutUrl("/admin/member/member_logout_confirm")
 				.logoutSuccessUrl("/");
 		return http.build();
 	}
