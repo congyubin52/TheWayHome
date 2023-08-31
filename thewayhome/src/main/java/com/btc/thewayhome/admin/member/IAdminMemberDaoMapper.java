7aabccd9e458b2fbf0aa87b1b61bf99953f833a1
iieunji023 <iieunji023@gmail.com>
2023-08-31 16:25:59 +0900

    feat: 관리자 리스트 승인 기능 구현
    - a_m_approval == 0일때 1로, a_m_approval == 1일때 2로 값 변경

--- thewayhome/src/main/java/com/btc/thewayhome/admin/member/IAdminMemberDaoMapper.java
+++ thewayhome/src/main/java/com/btc/thewayhome/admin/member/IAdminMemberDaoMapper.java
@@ -1,5 +1,11 @@
 package com.btc.thewayhome.admin.member;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
 public interface IAdminMemberDaoMapper {
 
     public int insertShelterNum(ShelterNumDto shelterNumDto);
@@ -14,6 +20,30 @@ public interface IAdminMemberDaoMapper {
 
     public List<AdminMemberDto> selectsSearchShelterName(String ShelterNo);
 
+    //회원가입 - 중복체크
+    public boolean isAdmin(String a_m_id);
 
+    //회원가입 - 계정생성
+    public void insertNewAccount(AdminMemberDto adminMemberDto);
+
+    //로그인
+    public AdminMemberDto selectAdminForLogin(Map<String, String> msgMap);
+
+    //회원 정보 수정
+    public int updateAccount(AdminMemberDto adminMemberDto);
+    // public int updateAccount(Map<String, String> msgMap);
+
+    //회원 정보 수정 - 최신화
+    public AdminMemberDto getLatestAccountInfo(AdminMemberDto adminMemberDto);
+    //public Map<String,Object> getLatestAccountInfo(Map<String, String> msgMap);
+
+    //회원탈퇴
+    public int deleteAccount(int a_m_no);
+
+    //관리자 정보 리스트
+    public List<AdminMemberDto> selectAdminForApproval();
+
+    //관리자 승인 처리
+    public void updateAdminForApporoval(int a_m_no);
 
 }
