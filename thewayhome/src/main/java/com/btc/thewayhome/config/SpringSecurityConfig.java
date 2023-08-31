4b7ea15ea01b0972a2d0fe6bb1fdd653ed7a5c7d
jinbeom <jinbeomgit@gmail.com>
2023-08-31 15:47:38 +0900

    pull 당기기 위한 push

--- thewayhome/src/main/java/com/btc/thewayhome/config/SpringSecurityConfig.java
+++ thewayhome/src/main/java/com/btc/thewayhome/config/SpringSecurityConfig.java
@@ -28,47 +28,47 @@ public class SpringSecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 
+//	@Bean
+//	@Order(1)
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		log.info("UserFilterChain");
+//
+//		http.csrf().disable()
+//				.cors().disable()
+//				.authorizeHttpRequests(request -> request
+//						.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+//						.requestMatchers("/css/**", "/error/**", "/img/**", "/js/**", "", "/",
+//								"/user/member/create_account_form", "/user/member/create_account_confirm").permitAll()   // Security 제외
+//						.anyRequest().authenticated()
+//				)
+//				.formLogin(login -> login                           	// 로그인 시 폼(form)을 이용
+//						.loginPage("/user/member/member_login_form")    // 로그인시 폼 주소 설정
+//						.loginProcessingUrl("/user/member/member_login_confirm")
+//						.usernameParameter("u_m_id")
+//						.passwordParameter("u_m_pw")
+//						.successHandler((request, response, authentication) -> {	// 로그인 성공시 이동 페이지 URI
+//							log.info("successHandler!!");
+//
+//							UserMemberDto userMemberDto = new UserMemberDto();
+//							userMemberDto.setU_m_id(authentication.getName());
+//							UserMemberDto loginedUserMemberDto = iUserMemberDaoMapper.selectUserMemberForLogin(userMemberDto);
+//
+//							HttpSession session = request.getSession();
+//							session.setAttribute("loginedUserMemberDto", loginedUserMemberDto);
+//							session.setMaxInactiveInterval(60 * 30);
+//
+//							response.sendRedirect("/");
+//
+//						})
+//						.permitAll())
+//				.logout()
+//				.logoutUrl("/user/member/member_logout_confirm")
+//				.logoutSuccessUrl("/");
+//		return http.build();
+//	}
+
 	@Bean
-	@Order(1)
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		log.info("UserFilterChain");
-
-		http.csrf().disable()
-				.cors().disable()
-				.authorizeHttpRequests(request -> request
-						.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-						.requestMatchers("/css/**", "/error/**", "/img/**", "/js/**", "", "/",
-								"/user/member/create_account_form", "/user/member/create_account_confirm").permitAll()   // Security 제외
-						.anyRequest().authenticated()
-				)
-				.formLogin(login -> login                           	// 로그인 시 폼(form)을 이용
-						.loginPage("/user/member/member_login_form")    // 로그인시 폼 주소 설정
-						.loginProcessingUrl("/user/member/member_login_confirm")
-						.usernameParameter("u_m_id")
-						.passwordParameter("u_m_pw")
-						.successHandler((request, response, authentication) -> {	// 로그인 성공시 이동 페이지 URI
-							log.info("successHandler!!");
-
-							UserMemberDto userMemberDto = new UserMemberDto();
-							userMemberDto.setU_m_id(authentication.getName());
-							UserMemberDto loginedUserMemberDto = iUserMemberDaoMapper.selectUserMemberForLogin(userMemberDto);
-
-							HttpSession session = request.getSession();
-							session.setAttribute("loginedUserMemberDto", loginedUserMemberDto);
-							session.setMaxInactiveInterval(60 * 30);
-
-							response.sendRedirect("/");
-
-						})
-						.permitAll())
-				.logout()
-				.logoutUrl("/user/member/member_logout_confirm")
-				.logoutSuccessUrl("/");
-		return http.build();
-	}
-
-	@Bean
-	@Order(2)
+//	@Order(2)
 	public SecurityFilterChain filterChainAdmin(HttpSecurity http) throws Exception {
 		log.info("filterChain");
 
@@ -77,7 +77,7 @@ public class SpringSecurityConfig {
 				.authorizeHttpRequests(request -> request
 						.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
 						.requestMatchers("/css/**", "/error/**", "/img/**", "/js/**", "", "/",
-								"/admin/member/create_account_form", "/admin/member/create_account_confirm").permitAll()   // Security 제외
+								"/admin/member/create_account_form", "/admin/member/create_account_confirm","/admin/member/searchShelterName").permitAll()   // Security 제외
 						.anyRequest().authenticated()
 				)
 				.formLogin(login -> login                           // 로그인 시 폼(form)을 이용
