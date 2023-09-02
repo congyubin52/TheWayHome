79963596a65b01b3ef93e9505b6439a150b10150
congyubin52 <congyubin52@gamil.com>
2023-09-02 22:56:02 +0900

    feat: 후기 게시판 글작성, 상세보기, 수정 페이지
    글작성 페이지 image 파일 제외 db 입력 완료
    상세보기 페이지 데이터 가져오기 완료
    수정 페이지 데이터 가져오기 완료

--- thewayhome/src/main/java/com/btc/thewayhome/user/board/review/ReviewBoardUserController.java
+++ thewayhome/src/main/java/com/btc/thewayhome/user/board/review/ReviewBoardUserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Log4j2
 @Controller
@@ -19,25 +20,25 @@ public class ReviewBoardUserController {
     ReviewBoardUserService reviewBoardUserService;
 
     /*
-        후기 게시판 페이지
+        후기 게시판 페이지 이동
      */
     @GetMapping("/review_board")
     public String reviewBoardPage() {
         log.info("[ReviewBoardUserController] reviewBoardPage()");
 
-        String nextPage = "/user/board/review_board";
+        String nextPage = "/user/board/review/review_board";
 
         return nextPage;
     }
 
     /*
-        후기 게시판 글 작성 페이지
+        후기 게시판 글 작성 페이지 이동(로그인 상태일 때만)
      */
     @GetMapping("/write_review_form")
     public String reviewBoardWritePage(HttpSession session) {
         log.info("[ReviewBoardUserController] reviewBoardWritePage()");
 
-        String nextPage = "/user/board/write_review_form";
+        String nextPage = "/user/board/review/write_review_form";
 
         if(session.getAttribute("loginedUserMemberDto") == null) {
             nextPage = "/user/member/member_login_form";
@@ -54,7 +55,7 @@ public class ReviewBoardUserController {
     public String writeReviewConfirm(HttpSession session, ReviewBoardUserDto reviewBoardUserDto) {
         log.info("[ReviewBoardUserController] writeReviewConfirm()");
 
-        String nextPage = "/user/board/review_board";
+        String nextPage = "/user/board/review/review_board";
 
         UserMemberDto loginedUserMemberDto = (UserMemberDto) session.getAttribute("loginedUserMemberDto");
 
@@ -63,7 +64,7 @@ public class ReviewBoardUserController {
         int result = reviewBoardUserService.writeReviewConfirm(reviewBoardUserDto);
 
         if(result <= 0) {
-            nextPage = "redirect:/user/board/write_review_form";
+            nextPage = "redirect:/user/board/review/write_review_form";
 
         }
 
@@ -71,11 +72,14 @@ public class ReviewBoardUserController {
 
     }
 
+    /*
+        후기 게시판 상세보기 페이지 이동(b_no로 해당 게시글 DTO 가져옴)
+     */
     @GetMapping("/review_detail")
-    public String reviewDetailPage(int b_no, Model model) {
+    public String reviewDetailPage(@RequestParam("b_no") int b_no, Model model) {
         log.info("[ReviewBoardUserController] reviewDetailPage()");
 
-        String nextPage = "/user/board/review_detail";
+        String nextPage = "/user/board/review/review_detail";
 
         ReviewBoardUserDto selectReviewDto = reviewBoardUserService.reviewDetailPage(b_no);
 
@@ -84,11 +88,14 @@ public class ReviewBoardUserController {
         return nextPage;
     }
 
+    /*
+        
+     */
     @GetMapping("/review_modify_form")
     public String reviewModifyPage(int b_no, Model model) {
         log.info("[ReviewBoardUserController] reviewModifyPage()");
 
-        String nextPage = "/user/board/review_modify_form";
+        String nextPage = "/user/board/review/review_modify_form";
 
         ReviewBoardUserDto selectReviewDto = reviewBoardUserService.reviewDetailPage(b_no);
 
