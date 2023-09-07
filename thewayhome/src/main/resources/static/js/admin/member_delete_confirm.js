d3d9345483c112f4426774b9dcd4e8dcb522eca2
iieunji023 <iieunji023@gmail.com>
2023-09-07 18:28:28 +0900

    pull 하기 위한 commit

--- thewayhome/src/main/resources/static/js/admin/member_delete_confirm.js
+++ thewayhome/src/main/resources/static/js/admin/member_delete_confirm.js
@@ -1,6 +1,11 @@
 function deleteConfirm() {
     console.log('deleteConfirm() CALLED!!');
 
-        confirm(`정말 삭제하시겠습니까?`);
+        var result = confirm("회원 탈퇴를 하시겠습니까?");
+        if(result) {
+            alert("탈퇴 처리되었습니다.");
+        } else {
+            alert("탈퇴 취소되었습니다.");
+        }
 
 }
